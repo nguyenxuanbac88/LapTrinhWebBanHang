96e0963310ced177c355b6b471d6086eed1b765c
LtThanh <lamtanthanh2005@gmail.com>
2024-10-16 13:02:58 +0700

    Test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace LapTrinhWebBanHang.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Hihi()
         {
             return View();
         }
