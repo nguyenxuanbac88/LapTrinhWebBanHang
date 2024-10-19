a3115521aa44c1a028c08bbae8959924c23e1f99
Trần Duy Khoa <khoa992005@gmail.com>
2024-10-20 00:05:21 +0700

    fix

---
+++ Controllers/Sign_inController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LapTrinhWebBanHang.Controllers
+{
+    public class Sign_inController : Controller
+    {
+        // GET: Sign_in
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //http /home/get/đăng kí
+        public ActionResult Sign_in()
+        {
+            return View();
+        }
+        //http /home/post/đăng kí
+       
+    }
+}
