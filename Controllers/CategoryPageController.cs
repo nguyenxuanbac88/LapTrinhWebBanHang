a1cead858a451bd64417f6a8b14ffb595bcb58e2
LtThanh <lamtanthanh2005@gmail.com>
2024-10-19 16:06:07 +0700

    push category page

---
+++ Controllers/CategoryPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LapTrinhWebBanHang.Controllers
+{
+    public class CategoryPageController : Controller
+    {
+        // GET: CategoryPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
