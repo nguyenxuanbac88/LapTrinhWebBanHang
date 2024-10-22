fe00f929df59e5435d9715372a078e8cdacf6295
nguyenxuanbac88 <xuanbacatym@gmail.com>
2024-10-23 01:52:48 +0700

    -Update constraint for entity famework +using (var db = new WebsiteEntities4()) +add file AdminController

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using LapTrinhWebBanHang.Models;
+using LapTrinhWebBanHang.Services;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace LapTrinhWebBanHang.Controllers
+{
+
+    public class AdminController : Controller
+    {
+        WebsiteEntities4 db = new WebsiteEntities4();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ManageProducts()
+        {
+            using (var db = new WebsiteEntities4())
+            {
+                if (Session["IdUser"] == null)
+                {
+                    return RedirectToAction("Login", "Sign_in");
+                }
+                var userId = (int)Session["IdUser"];
+                var user = db.Users.SingleOrDefault(u => u.IdUser == userId);
+
+                if (user == null || user.IsAdmin != 1)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                var products = db.Products.ToList();
+                return View(products);
+            }
+        }
+    }
+}
--- Controllers/HomePageController.cs
+++ Controllers/HomePageController.cs
@@ -21,31 +21,35 @@ namespace LapTrinhWebBanHang.Controllers
         [HttpPost]
         public ActionResult Sign_in(string user, string password)
         {
-            WebsiteEntities4 db= new WebsiteEntities4();
-            string md5password = Md5.GetMd5Hash(password);
-            // Tìm người dùng trong cơ sở dữ liệu
-            var userInDb = db.Users.FirstOrDefault(u => u.Email.ToLower() == user.ToLower() && u.PasswordHash == md5password);
-            if (user.Length >=10 && user.Length < 35) 
+            using (var db = new WebsiteEntities4())//Mình update thêm cái using vô để nó được giải phóng khi sử dụng xong
             {
-                if (password.Length>=6 && password.Length<40)
+                string md5password = Md5.GetMd5Hash(password);
+                // Tìm người dùng trong cơ sở dữ liệu
+                var userInDb = db.Users.FirstOrDefault(u => u.Email.ToLower() == user.ToLower() && u.PasswordHash == md5password);
+                if (user.Length >= 10 && user.Length < 35)
                 {
-                    if (userInDb != null)
+                    if (password.Length >= 6 && password.Length < 40)
                     {
-                        Session["user"] = userInDb;
-                        return RedirectToAction("Home_page");
-                    }
-                    else
-                    {
-                        ModelState.AddModelError("", "Tài khoản hoặc mật khẩu không đúng.");
-                        return View();
+                        if (userInDb != null)
+                        {
+                            var userId = userInDb.IdUser; // Lấy IdUser của người dùng
+                            Session["IdUser"] = userId;
+                            Session["Email"] = userInDb;
+                            return RedirectToAction("Home_page");
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "Tài khoản hoặc mật khẩu không đúng.");
+                            return View();
+                        }
                     }
                 }
+                else
+                {
+                    return RedirectToAction("Sign_in");
+                }
+                return View();
             }
-            else
-            {
-                return RedirectToAction("Sign_in");
-            }
-            return View();
         }
 
         public ActionResult Logout()
