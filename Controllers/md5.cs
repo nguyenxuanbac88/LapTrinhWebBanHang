74d5b82af2ea806f5279f09da34c3ef5fad52c1f
nguyenxuanbac88 <xuanbacatym@gmail.com>
2024-10-22 14:37:02 +0700

    -update md5 hash-Update file SQL database -Fix Database

---
+++ Controllers/md5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace LapTrinhWebBanHang.Controllers
+{
+    public class md5
+    {
+        public static string GetMd5Hash(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
