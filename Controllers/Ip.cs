7f790c0e749296daa54b3decaf82999c7af72b28
nguyenxuanbac88 <xuanbacatym@gmail.com>
2024-10-22 22:49:42 +0700

    -Fix class

---
+++ Controllers/Ip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LapTrinhWebBanHang.Controllers
+{
+    public class Ip
+    {
+        public string GetUserIP()
+        {
+            string ipAddress = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            if (string.IsNullOrEmpty(ipAddress))
+            {
+                ipAddress = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+            }
+
+            return ipAddress;
+        }
+    }
+}
