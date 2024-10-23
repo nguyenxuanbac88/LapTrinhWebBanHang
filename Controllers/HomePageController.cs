bc5409e65f4c9291748c602a3e459a1c1049f9c2
Trần Duy Khoa <khoa992005@gmail.com>
2024-10-23 14:28:40 +0700

    Fix Sign_in and create Register

--- Controllers/HomePageController.cs
+++ Controllers/HomePageController.cs
@@ -13,50 +13,7 @@ namespace LapTrinhWebBanHang.Controllers
         {
             return View();
         }
-        public ActionResult Sign_in()
-        {
-            return View();
-        }
+       
 
-        [HttpPost]
-        public ActionResult Sign_in(string user, string password)
-        {
-            using (var db = new WebsiteEntities4())//Mình update thêm cái using vô để nó được giải phóng khi sử dụng xong
-            {
-                string md5password = Md5.GetMd5Hash(password);
-                // Tìm người dùng trong cơ sở dữ liệu
-                var userInDb = db.Users.FirstOrDefault(u => u.Email.ToLower() == user.ToLower() && u.PasswordHash == md5password);
-                if (user.Length >= 10 && user.Length < 35)
-                {
-                    if (password.Length >= 6 && password.Length < 40)
-                    {
-                        if (userInDb != null)
-                        {
-                            var userId = userInDb.IdUser; // Lấy IdUser của người dùng
-                            Session["IdUser"] = userId;
-                            Session["Email"] = userInDb;
-                            return RedirectToAction("Home_page");
-                        }
-                        else
-                        {
-                            ModelState.AddModelError("", "Tài khoản hoặc mật khẩu không đúng.");
-                            return View();
-                        }
-                    }
-                }
-                else
-                {
-                    return RedirectToAction("Sign_in");
-                }
-                return View();
-            }
-        }
-
-        public ActionResult Logout()
-        {
-            Session.Remove("user");
-            FormsAuthentication.SignOut();
-            return RedirectToAction("Sign_in");
-        }
     }
 }
--- Controllers/Sign_inController.cs
+++ Controllers/Sign_inController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using LapTrinhWebBanHang.Models;
+using LapTrinhWebBanHang.Services;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
 
 namespace LapTrinhWebBanHang.Controllers
 {
@@ -15,6 +19,45 @@ namespace LapTrinhWebBanHang.Controllers
             return View();
         }
         //http /home/post/đăng kí
-       
+
+        [HttpPost]
+        public ActionResult Sign_in(string user, string password)
+        {
+            WebsiteEntities4 db= new WebsiteEntities4();
+            {
+                string md5password = Md5.GetMd5Hash(password);
+                // Tìm người dùng trong cơ sở dữ liệu
+                var userInDb = db.Users.FirstOrDefault(u => u.Email.ToLower() == user.ToLower() && u.PasswordHash == md5password);
+                if (user.Length >= 10 && user.Length < 35)
+                {
+                    if (password.Length >= 6 && password.Length < 40)
+                    {
+                        if (userInDb != null)
+                        {
+                           
+                            Session["Email"] = userInDb;
+                            return RedirectToAction("Home_page","HomePage");
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "Tài khoản hoặc mật khẩu không đúng.");
+                            return View();
+                        }
+                    }
+                }
+                else
+                {
+                    return RedirectToAction("Sign_in");
+                }
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Remove("Email");
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Sign_in");
+        }
     }
 }
