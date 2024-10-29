0bbb5c67cd8ae448fafc706037cab78790c13e7b
nguyenxuanbac88 <xuanbacatym@gmail.com>
2024-10-29 13:47:54 +0700

    -Fix logout sessjon clear

--- Controllers/HomePageController.cs
+++ Controllers/HomePageController.cs
@@ -1,6 +1,8 @@
 ﻿using LapTrinhWebBanHang.Models;
 using LapTrinhWebBanHang.Services;
+using System;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 
@@ -11,6 +13,10 @@ namespace LapTrinhWebBanHang.Controllers
         // GET: HomePage
         public ActionResult Home_page()
         {
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetExpires(DateTime.UtcNow.AddDays(-1));
+            Response.Cache.SetNoStore();
+
             return View();
         }
        
