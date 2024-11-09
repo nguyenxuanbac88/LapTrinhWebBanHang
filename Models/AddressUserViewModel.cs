94973b634563f7b9dc911385f466a93c2ae245f5
Trần Duy Khoa <khoa992005@gmail.com>
2024-11-09 15:49:11 +0700

    update shopping cart

---
+++ Models/AddressUserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LapTrinhWebBanHang.Models
+{
+    public class AddressUserViewModel
+    {
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Province { get; set; }
+        public string Town { get; set; }
+        public string Block { get; set; }
+        public string SpecificAddress { get; set; }
+    }
+}
--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -7,9 +7,10 @@ namespace LapTrinhWebBanHang.Models
 {
     public class CartItem
     {
-        public int ProductID { get; set; }        // ID của sản phẩm
-        public string ProductName { get; set; }   // Tên sản phẩm
-        public decimal Price { get; set; }        // Giá sản phẩm
-        public int Quantity { get; set; }         // Số lượng sản phẩm trong giỏ hàng
+        public int ProductID { get; set; }
+        public string ProductName { get; set; } // Thêm tên sản phẩm
+        public decimal Price { get; set; } // Thêm giá sản phẩm
+        public int Quantity { get; set; }
+        public string ImageUrl { get; set; } // Tuỳ chọn: Thêm URL hình ảnh
     }
 }
