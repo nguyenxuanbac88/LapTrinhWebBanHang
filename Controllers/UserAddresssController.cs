5f43e87fa0e652c277ae2f387494c10c7c23d844
Trần Duy Khoa <khoa992005@gmail.com>
2024-11-09 17:03:33 +0700

    create account user

--- Controllers/UserAddresssController.cs
+++ Controllers/UserAddresssController.cs
@@ -1,54 +1,23 @@
 ﻿using LapTrinhWebBanHang.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace LapTrinhWebBanHang.Controllers
 {
     public class UserAddresssController : Controller
     {
-        private WebsiteEntities4 db = new WebsiteEntities4();
-
-        // Hiển thị thông tin người dùng và địa chỉ
+        // GET: UserAddresss/Index
         public ActionResult Index()
         {
-            // Lấy thông tin người dùng hiện tại từ session hoặc cookie
-            string email = User.Identity.Name;
-            var user = db.Users.FirstOrDefault(u => u.Email == email);
-            if (user == null)
+            string email = Session["Email"] as string;
+            if (string.IsNullOrEmpty(email))
             {
-                return HttpNotFound();
+                return RedirectToAction("Sign_in", "Account");
             }
 
-            var address = db.AddressUsers.FirstOrDefault(a => a.IdUser == user.IdUser);
-            if (address == null)
+            using (WebsiteEntities4 db = new WebsiteEntities4())
             {
-                return HttpNotFound();
-            }
-
-            // Trả về view với thông tin người dùng và địa chỉ
-            var viewModel = new AddressUserViewModel
-            {
-                FullName = address.FullName,
-                Phone = address.Phone,
-                Province = address.Province,
-                Town = address.Town,
-                Block = address.Block,
-                SpecificAddress = address.SpecificAddress
-            };
-            return View(viewModel);
-        }
-
-        // Cập nhật thông tin địa chỉ người dùng
-        [HttpPost]
-        public ActionResult Update(AddressUserViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                // Lấy thông tin người dùng hiện tại từ session hoặc cookie
-                string email = User.Identity.Name;
                 var user = db.Users.FirstOrDefault(u => u.Email == email);
                 if (user == null)
                 {
@@ -56,24 +25,61 @@ namespace LapTrinhWebBanHang.Controllers
                 }
 
                 var address = db.AddressUsers.FirstOrDefault(a => a.IdUser == user.IdUser);
-                if (address == null)
+                var viewModel = new AddressUserViewModel
                 {
-                    return HttpNotFound();
+                    IdUser = user.IdUser,
+                    FullName = address?.FullName ?? "",
+                    Phone = address?.Phone ?? "",
+                    Province = address?.Province ?? "",
+                    Town = address?.Town ?? "",
+                    Block = address?.Block ?? "",
+                    SpecificAddress = address?.SpecificAddress ?? ""
+                };
+
+                return View(viewModel);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Update(AddressUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string email = Session["Email"] as string;
+                if (string.IsNullOrEmpty(email))
+                {
+                    return RedirectToAction("Sign_in", "Account");
                 }
 
-                // Cập nhật thông tin địa chỉ
-                address.FullName = model.FullName;
-                address.Phone = model.Phone;
-                address.Province = model.Province;
-                address.Town = model.Town;
-                address.Block = model.Block;
-                address.SpecificAddress = model.SpecificAddress;
+                using (WebsiteEntities4 db = new WebsiteEntities4())
+                {
+                    var user = db.Users.FirstOrDefault(u => u.Email == email);
+                    if (user == null)
+                    {
+                        return HttpNotFound();
+                    }
 
-                db.SaveChanges();
-                ModelState.AddModelError("", "Cập nhật địa chỉ thành công");
+                    var address = db.AddressUsers.FirstOrDefault(a => a.IdUser == user.IdUser);
+                    if (address == null)
+                    {
+                        address = new AddressUser { IdUser = user.IdUser };
+                        db.AddressUsers.Add(address);
+                    }
+
+                    address.FullName = model.FullName;
+                    address.Phone = model.Phone;
+                    address.Province = model.Province;
+                    address.Town = model.Town;
+                    address.Block = model.Block;
+                    address.SpecificAddress = model.SpecificAddress;
+
+                    db.SaveChanges();
+                    TempData["SuccessMessage"] = "Cập nhật thông tin thành công";
+                }
             }
 
-            return View("Index", model);
+            return RedirectToAction("Index");
         }
+
     }
 }
--- Models/AddressUserViewModel.cs
+++ Models/AddressUserViewModel.cs
@@ -7,6 +7,7 @@ namespace LapTrinhWebBanHang.Models
 {
     public class AddressUserViewModel
     {
+        public int IdUser { get; set; }
         public string FullName { get; set; }
         public string Phone { get; set; }
         public string Province { get; set; }
@@ -14,4 +15,5 @@ namespace LapTrinhWebBanHang.Models
         public string Block { get; set; }
         public string SpecificAddress { get; set; }
     }
+
 }
