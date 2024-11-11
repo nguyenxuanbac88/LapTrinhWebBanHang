66500e8ea8a1b197a2a85fbca1bf8ccb5b827431
nguyenxuanbac88 <xuanbacatym@gmail.com>
2024-11-11 14:53:56 +0700

    -FIx type int price

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -9,7 +9,7 @@ namespace LapTrinhWebBanHang.Models
     {
         public int ProductID { get; set; }  // ID sản phẩm
         public string ProductName { get; set; }  // Tên sản phẩm
-        public decimal Price { get; set; }  // Giá sản phẩm
+        public int Price { get; set; }  // Giá sản phẩm
         public int Quantity { get; set; }  // Số lượng sản phẩm
         public string Size { get; set; }
         public string ImageUrl { get; set; }  // URL hình ảnh sản phẩm
